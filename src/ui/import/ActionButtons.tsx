6bfe764f5fac8cbcff0d50b18a5e993fc73e6ccf
Baker Bokorney <bbokorney@gmail.com>
2023-01-14 18:08:16 -0700

    Disable back and next buttons at ends of transaction list

--- src/ui/import/ActionButtons.tsx
+++ src/ui/import/ActionButtons.tsx
@@ -5,12 +5,16 @@ import { ArrowForward, ArrowBack } from "@mui/icons-material";
 import {
   nextTransaction,
   previousTransaction,
+  selectCurrentImportTransaction,
+  selectImportTransactions,
 } from "../../lib/import/importSlice";
-import { useAppDispatch } from "../../lib/store/hooks";
+import { useAppDispatch, useAppSelector } from "../../lib/store/hooks";
 import SaveButton from "./SaveButton";
 
 const ActionButtons = () => {
   const dispatch = useAppDispatch();
+  const transaction = useAppSelector(selectCurrentImportTransaction);
+  const { transactionsIndex } = useAppSelector(selectImportTransactions);
 
   return (
     <Stack direction="row" spacing={1}>
@@ -19,6 +23,7 @@ const ActionButtons = () => {
         variant="contained"
         onClick={() => dispatch(previousTransaction())}
         startIcon={<ArrowBack />}
+        disabled={transactionsIndex === 0}
       >
         Back
       </Button>
@@ -30,8 +35,9 @@ const ActionButtons = () => {
         variant="contained"
         onClick={() => dispatch(nextTransaction("skipped"))}
         endIcon={<ArrowForward />}
+        disabled={!transaction}
       >
-        Skip
+        {transaction?.actionTaken === "skipped" ? "Next" : "Skip"}
       </Button>
     </Stack>
   );
